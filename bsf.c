ac1a95bf2aa702c3c591c0e4947041c00c422c8f
doremi <nothing>
2010-06-29 09:19:33 +0800

    bsf 目前看來可以正常運作

--- bsf.c
+++ bsf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdint.h>
 
-static uint32_t map = 0xffffffff;
+uint32_t map = 0xffffffff;
 
 uint32_t __attribute__((noinline)) alloc()
 {
 	uint32_t ret;
 	__asm__ ("bsr %0, %1\n\t"\
 		"btr %1, %0\n\t"
-		:"=r"(map), "=r"(ret));
+		::"m"(map), "r"(ret));
 
 	return ret;
 }
@@ -16,15 +16,10 @@ uint32_t __attribute__((noinline)) alloc()
 int main()
 {
 	int i;
-//	for (i = 0; i < 32; ++i) {
-	uint32_t r = alloc();
+	for (i = 0; i < 32; ++i) {
+		uint32_t r = alloc();
 		printf("%d\n", r);
 		printf("map: 0x%x\n", map);
-
-		r = alloc();
-                printf("%d\n", r);
-                printf("map: 0x%x\n", map);
-
-//	}
+	}
 	return 0;
 }
