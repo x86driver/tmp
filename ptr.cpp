1b5301b1a7985efe5dbccd75f1bcbe31abf509b7
doremi <nothing>
2010-12-15 15:26:30 +0800

    Execute() 也必須是要 typename 以避免找不到對應的型別

--- ptr.cpp
+++ ptr.cpp
@@ -8,7 +8,8 @@ struct binary_function {
 };
 
 template <typename S, typename T, typename A, typename B>
-class mem_fun2_t : public binary_function<T*, A, S> {
+class mem_fun2_t {
+// : public binary_function<T*, A, S> {
 public:
         explicit mem_fun2_t(S (T::*pf)(A, B)) : f(pf) {}
         S operator()(T* p, A x, B y) const { return (p->*f)(x, y); }
@@ -21,16 +22,16 @@ mem_fun2_t<S,T,A,B> mem_fun(S (T::*f)(A, B)) {
         return mem_fun2_t<S,T,A,B>(f);
 }
 
-template <typename A, typename Function>
-void Execute(A *a, Function f, int x, int y)
+template <typename A, typename Function, typename X, typename Y>
+void Execute(A *a, Function f, X x, Y y)
 {
         f(a, x, y);
 }
 
 class TPolicy {
 public:
-        void show(int a, int b) {
-                printf("I'm TPolicy, %d, %d\n", a, b);
+        void show(void *b, int a) {
+                printf("I'm TPolicy, %d, %d\n", a, *(int*)b);
         }
 };
 
@@ -38,9 +39,10 @@ template <typename T = TPolicy>
 class Daemon : public T {
 //class Daemon {
 public:
-        void go(int a, int b) {
-                T::show(a, b);
+        void go(void *b, int a) {
+//                T::show(a, b);
 //                printf("%d\n", a);
+		Execute(this, mem_fun(&T::show), b, a);
         }
 };
 
@@ -48,8 +50,12 @@ int main()
 {
         Daemon<> d;
 //        d = new Daemon<>;
-        Execute(&d, mem_fun(&Daemon<>::go), 10, 20);
+//        Execute(&d, mem_fun(&Daemon<>::go), 10, 20);
 //        delete d;
+	int num = 1234;
+	int *buf = &num;
+	d.go(buf, 40);
+	printf("%s\n", __TIMESTAMP__);
         return 0;
 }
 
